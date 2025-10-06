6b898505afe0295d161726b7d9105f9de2d4d3bd
Ruly Tafzil <rulytafzil+git@gmail.com>
2025-10-06 16:41:49 -0700

    Added Doom Emacs configs

---
+++ .config/doom/custom.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '((output-pdf "Zathura") ((output-dvi has-no-display-manager) "dvi2tty")
+     ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
+     (output-pdf "Evince") (output-html "xdg-open"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
